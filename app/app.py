c443a28fd08504376781b5c96fb90a0eb7c8457d
CarterSnich <jmestreraclosa@gmail.com>
2021-04-08 23:33:26 +0800

    Added reader; added more doujin details; redined UI - April 8, 2021 11:33:23 PM

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, jsonify
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+# from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from hentai import Hentai, Format
 
 # Removed for the reason that retrieving image from nHentai on Android is now working.
@@ -42,16 +42,37 @@ def nuke_codes():
                 'title_pretty' : doujin.title(Format.Pretty),
                 'tags' : [tag.name for tag in doujin.tag],
                 'poster_link' : doujin.image_urls[0],
+                # 'poster_blob' : poster_blob,
                 'artist' : [{
                     'name' : artist.name,
-                    'url' : artist.url
-                } for artist in doujin.artist]
-                # 'poster_blob' : poster_blob
+                    'url' : artist.url,
+                    'count' : artist.count
+                } for artist in doujin.artist],
+                'languages' : [lang.name for lang in doujin.language],
+                'categories' : [cat.name for cat in doujin.category],
+                'pages' : doujin.num_pages,
+                'uploaded' : doujin.upload_date
             }
         else:
             return None
     return None
 
+# reading doujin with lite reader
+@app.route('/read_manga', methods=['GET'])
+def read_manga():
+    code = request.args.get('code') 
+    if code:
+        print(f"READER: {code}")
+
+        if (Hentai.exists(code)):
+            doujin = Hentai(code)
+            return render_template('includes/reader.html', img_urls=enumerate(doujin.image_urls))
+        else:
+            return render_template('includes/reader.html', img_urls=[])
+
+    return redirect('/')
+
+    
 
 if __name__ == '__main__':
     app.secret_key = '2500'
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -6,7 +6,7 @@ function rmDuple(array) {
         let uniqueArray = [];
 
         // Loop through array values
-        for (i = 0; i < array.length; i++) {
+        for (let i = 0; i < array.length; i++) {
             if (uniqueArray.indexOf(array[i]) === -1) {
                 uniqueArray.push(array[i]);
             }
@@ -19,6 +19,11 @@ function rmDuple(array) {
 
 $(document).ready(function () {
     console.log('DOM ready.');
+
+    // bind tooltips to elements
+    $(document).tooltip({
+        selector : '[data-toggle="tooltip"]'
+    })
 
     $('form').on('submit', function (e) {
         e.preventDefault(); // prevents the form to action by itself
@@ -49,31 +54,91 @@ $(document).ready(function () {
                         console.log(data);
 
                         let doujin = data;
-                        let tags_div = document.createElement('div');
 
-                        doujin['tags'].forEach((tag, i) => {
-                            $(tags_div).append(`<small class="badge badge-secondary mr-1">${tag}</small>`)
-                        })
+                        let tags_div = function () {
+                            let html = document.createElement('div');
+                            html.className = "ml-2";
+                            doujin['tags'].forEach((tag, i) => {
+                                $(html).append(`<small class="badge badge-secondary mr-1">${tag}</small>`)
+                            })
+                            return html.outerHTML;
+                        }()
+
+                        let artist_div = function () {
+                            let html = document.createElement('div');
+                            html.className = "ml-2";
+                            doujin['artist'].forEach((artist, i) => {
+                                $(html).append(`
+                                    <a class="mr-1 d-inline-flex badge p-0" href="${artist['url']}">
+                                        <span class="p-1 rounded-left">${artist['name']}</span>
+                                        <span class="p-1 rounded-right">${artist['count']}</span>
+                                    </a>
+                                `);
+                            })
+                            return html.outerHTML;
+                        }()
+
+                        let languages = function () {
+                            let html = document.createElement('div');
+                            html.className = "ml-2";
+                            doujin['languages'].forEach((lang, i) => {
+                                $(html).append(`<small class="badge badge-secondary mr-1">${lang}</small>`)
+                            })
+                            return html.outerHTML;
+                        }()
+
+                        let categories = function () {
+                            let html = document.createElement('div');
+                            html.className = "ml-2";
+                            doujin['categories'].forEach((cat, i) => {
+                                $(html).append(`<small class="badge badge-secondary mr-1">${cat}</small>`)
+                            })
+                            return html.outerHTML;
+                        }()
 
                         $('#aftermath-list ul.list-group').append(`
-                            <li id="${doujin['id']}" class="list-group-item list-group-item-${index % 2 == 0 ? 'light' : 'dark'} list-group-item-action rounded-sm mb-1" href="https://nhentai.net/g/${doujin['id']}">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <img src="${doujin['poster_link']}" class="img-fluid img-thumbnail" width="80%">
-                                    <div>
-                                        <code class="text-light badge badge-dark">#${doujin['id']}</code>
+                            <li id="${doujin['id']}" class="list-group-item list-group-item-${index % 2 == 0 ? 'light' : 'dark'} list-group-item-action rounded-sm mb-1">
+                                <div class="d-flex w-100">
+                                    <a href="/read_manga?code=${doujin['id']}">
+                                        <img src="${doujin['poster_link']}" class="img-fluid img-thumbnail" width="100%"  data-toggle="tooltip" data-placement="top" title="Read with lite reader">
+                                    </a>
+                                    <div class="w-100 ml-3">
+                                        <code class="badge badge-secondary font-weight-bold">#${doujin['id']}</code>
                                     </div>
                                 </div>
                                 <div class="w-100">
-                                    <p class="mb-1">${doujin['title_pretty']}</p>
+                                    <div class="w-100">
+                                        <a class="btn btn-link text-left m-0 p-0 border-0" href="https://nhentai.net/g/${doujin['id']}" data-toggle="tooltip" data-placement="top" title="View on nHentai">
+                                            ${doujin['title_pretty']}
+                                            <span class="glyphicon glyphicon-new-window"></span>
+                                        </a>
+                                    </div>
                                     <small class="text-muted mb-1">${doujin['title_release']}</small>
                                     <div class="w-100 doujin-details">
-                                        <div class="w-100 tags">
-                                            Tags: ${$(tags_div).html()}
+                                        <div class="doujin-tags d-inline-flex w-100 mt-1">
+                                            Tags: 
+                                            ${tags_div}
                                         </div>
-                                        <span>Artist(s): <small class="badge badge-secondary mr-1">${'artist'}</small></span>
-                                        
+                                        <div class="doujin-artists d-inline-flex w-100 mt-1">
+                                            Artists: 
+                                            ${artist_div}
+                                        </div>
+                                        <div class="doujin-languages d-inline-flex w-100 mt-1">
+                                            Languages: 
+                                            ${languages}
+                                        </div>
+                                        <div class="doujin-categories d-inline-flex w-100 mt-1">
+                                            Categories: 
+                                            ${categories}
+                                        </div>
+                                        <div class="doujin-categories d-inline-flex w-100 mt-1">
+                                            Pages:
+                                            <small class="badge badge-secondary ml-2">${doujin['pages']}</small>
+                                        </div>
+                                        <div class="doujin-categories d-inline-flex w-100 mt-1">
+                                            Uploaded: <span class="text-primary ml-2">${doujin['uploaded']}</span>
+                                        </div>
                                     </div>
-                                    
                                 </div>
 
                             </li>
@@ -81,22 +146,15 @@ $(document).ready(function () {
                     })
                     .catch((err) => { console.log(`${code} : ${err}`) })
             })
+
+
+            $('#aftermath-list ul.list-group').on('click', 'li div a', function (e) {
+                e.preventDefault();
+                window.open(this.getAttribute('href'));
+            })
         }
 
     })
 
-    // $('#aftermath-list ul').on('mouseenter', function () {
-    //     $(this).toggleClass("border border-info")
-    // }, function () {
-    //     console.log('fuck off!')
-    // })
-
-    // $('#aftermath-list ul li').on({
-    //     mouseenter: function () {
-    //         $(this).addClass("border border-info");
-    //     }, mouseleave: function () {
-    //         $(this).removeClass("border border-info");
-    //     }
-    // });
 
 })
